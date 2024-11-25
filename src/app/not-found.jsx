8b463fd707092737d7a12d5ece2b377538467b47
Andre Da Silva Carreira <andreecarreira@gmail.com>
2024-11-25 11:35:28 +0000

    Add CSS

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,6 +1,6 @@
 //Page not found
 
-export default function PageNotFound() {
+export default function NotFound() {
   return (
     <h2>
       Page not found! Please return to a different page using the navigation bar
